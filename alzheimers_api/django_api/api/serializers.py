b026ddc2fef4c9d6d51725e9c5f738947da5f843
zein moualla <caizer1400@gmail.com>
2024-12-02 22:41:44 +0100

    Managed to make it work locally. THe Django Rest Framework manages data sent from the Fontend and gives it ti a View. Nex step, is to create the Predict Function, then deploy the App on BeansTalk.

--- alzheimers_api/django_api/api/serializers.py
+++ alzheimers_api/django_api/api/serializers.py
@@ -6,3 +6,14 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+
+# serializers.py
+from rest_framework import serializers
+
+class PatientInputSerializer(serializers.Serializer):
+    age = serializers.IntegerField(min_value=0, max_value=120, required=True)
+    bmi = serializers.FloatField(min_value=0, required=True)
+    smoker = serializers.BooleanField(required=True)
+    memory_problems = serializers.BooleanField(required=True)
+    family_history = serializers.CharField(max_length=100, required=True)
